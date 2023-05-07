305b85468e46c8343172af28946c92a28f733636
steverova <steverova0594@gmail.com>
2023-05-07 03:22:07 -0600

    movie by id

--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -3,6 +3,7 @@ import { Link, useParams } from "react-router-dom";
 import { useMovie } from "../hooks/useMovie";
 import ActorList from "../components/ActorList";
 import Badges from "../components/Badges";
+import "../assets/loader.css";
 
 export default function MovieInfo() {
   let { title_id } = useParams();
@@ -10,80 +11,92 @@ export default function MovieInfo() {
   const { movie, getMovie } = useMovie(title_id);
 
   useEffect(() => {
-    getMovie();
+    setTimeout(() => {
+      getMovie();
+    }, 1000);
   }, []);
 
   return (
     <>
-      <div style={{padding: "60px 50px 60px 0px"}} className="container-fluid ">
-        <div style={{ marginTop: "20px" }} className="row">
-          <article>
-            <p style={{ fontSize: "30px", textAlign: "center" }}>
-              {movie.title} ({movie.year})
-            </p>
-          </article>
+      {movie.length !== 0 ? (
+        <div
+          style={{ padding: "60px 50px 60px 0px" }}
+          className="container-fluid "
+        >
+          <div style={{ marginTop: "20px" }} className="row">
+            <article>
+              <p style={{ fontSize: "30px", textAlign: "center" }}>
+                {movie.title} ({movie.year})
+              </p>
+            </article>
 
-          <article style={{ marginTop: "10px" }}>
-            <div className="row">
-              <div
-                style={{
-                  textAlign: "center",
-                  marginTop: "20px",
-                  marginBottom: "20px",
-                }}
-                className="col-md-6"
-              >
-                <img src={movie.poster} alt={movie.title} />
-              </div>
+            <article style={{ marginTop: "10px" }}>
+              <div className="row">
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "20px",
+                    marginBottom: "20px",
+                  }}
+                  className="col-md-6"
+                >
+                  <img src={movie.poster} alt={movie.title} />
+                </div>
 
-              <div className="col-md-6">
-                <div style={{ marginTop: "30px" }}>
-                  <p style={{ fontSize: "1.1em" }}>{movie.plot}</p>
+                <div className="col-md-6">
+                  <div style={{ marginTop: "30px" }}>
+                    <p style={{ fontSize: "1.1em" }}>{movie.plot}</p>
 
-                  <hr></hr>
+                    <hr></hr>
 
-                  {movie && (
-                    <div>
-                      <ActorList
-                        title={"Director"}
-                        actors={movie.director}
-                      ></ActorList>
-                      <ActorList
-                        title={"Writer"}
-                        actors={movie.writer}
-                      ></ActorList>
-                      <ActorList
-                        title={"Actors"}
-                        actors={movie.actors}
-                      ></ActorList>
+                    {movie && (
+                      <div>
+                        <ActorList
+                          title={"Director"}
+                          actors={movie.director}
+                        ></ActorList>
+                        <ActorList
+                          title={"Writer"}
+                          actors={movie.writer}
+                        ></ActorList>
+                        <ActorList
+                          title={"Actors"}
+                          actors={movie.actors}
+                        ></ActorList>
+                      </div>
+                    )}
+
+                    <hr></hr>
+
+                    <div style={{ marginTop: "10px" }}>
+                      <Badges
+                        badges={[
+                          movie.rated,
+                          movie.released,
+                          movie.runtime,
+                          movie.country,
+                          movie.language,
+                          movie.genre,
+                        ]}
+                      ></Badges>
                     </div>
-                  )}
-
-                  <hr></hr>
-
-                  <div style={{ marginTop: "10px" }}>
-                    <Badges
-                      badges={[
-                        movie.rated,
-                        movie.released,
-                        movie.runtime,
-                        movie.country,
-                        movie.language,
-                        movie.genre,
-                      ]}
-                    ></Badges>
                   </div>
                 </div>
               </div>
-            </div>
-          </article>
-          <Link to="/">
-            <article style={{ marginTop: "20px", textAlign: "center" }}>
-              <i className="fa-solid fa-house fa-2x"></i>
             </article>
-          </Link>
+            <Link to="/">
+              <article style={{ marginTop: "20px", textAlign: "center" }}>
+                <i className="fa-solid fa-house fa-2x"></i>
+              </article>
+            </Link>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="container-fluid center">
+
+          <span className="loader"></span>
+        </div>
+      )}
     </>
   );
 }
